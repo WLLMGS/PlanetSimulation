b0c45a685ccee24545af38a3ddec70febc29cdf7
WLLMGS <willem.goos@hotmail.com>
2018-11-27 14:21:21 +0100

    Initial commit

---
+++ Assets/Scripts/PlanetAttractorScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetAttractorScript : MonoBehaviour
+{
+	private static PlanetAttractorScript _instance = null;
+
+	public static PlanetAttractorScript Instance
+	{
+		get{
+			return _instance;
+		}
+	}
+
+
+	void Awake()
+	{
+		if(_instance == null) _instance = this;
+	}
+
+
+	private float _gravity = -20f;
+
+	public void Attract(Transform other)
+	{
+		Vector3 dir = other.position - transform.position;
+		Vector3 updir = other.up;
+
+		dir.Normalize();
+
+		other.GetComponent<Rigidbody>().AddForce(_gravity * dir);
+
+		other.rotation = Quaternion.FromToRotation(updir, dir) * other.rotation;
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody _rb;
+    private PlanetAttractorScript _attractor;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _attractor = PlanetAttractorScript.Instance;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _attractor.Attract(transform);
+
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(transform.up * 1.0f, ForceMode.Impulse);
+        }
+
+        float yaxis = Input.GetAxis("Vertical");
+        float xaxis = Input.GetAxis("Horizontal");
+
+        _rb.AddForce(yaxis * transform.forward * 20.0f);
+        _rb.AddForce(xaxis * transform.right * 20.0f);
+    }
+}
